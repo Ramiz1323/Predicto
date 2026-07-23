f4d2d5e4a16372e6f0afb15df0b6fd151f9d3f12
Sk Ramiz Raza <reenabibi5678@gmail.com>
2026-07-24 00:59:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import EmotionDetector from './Features/EmotionDetector'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to My React App</h1>
+      <EmotionDetector /> 
+    </>
+  )
+}
+
+export default App
